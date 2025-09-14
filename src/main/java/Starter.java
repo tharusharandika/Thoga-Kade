58f8d28b476a76973374b42ca8243ac3d0ccade1
tharusharandika <tharusharandika7788@gmail.com>
2025-09-15 02:04:17 +0530

    Added DashboardForm

---
+++ src/main/java/Starter.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
